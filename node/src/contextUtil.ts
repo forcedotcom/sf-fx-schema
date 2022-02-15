5d69c61b2a6c81d10f4d691e413bcee42c538a10
D. J. Hagberg <dhagberg@salesforce.com>
2022-02-15 16:28:08 -0700

    Working node tests of SfContext, SfFnContext *with* deadline, and ResponseExtraInfo

--- node/src/contextUtil.ts
+++ node/src/contextUtil.ts
@@ -20,7 +20,12 @@ import { SfFnContext } from "./sfFnContext";
 
 export class ContextUtil {
 
-    public static decodeSfContext(base64Str: string): SfContext {
+    /**
+     * Decode a SfContext from its base64-encoded JSON.
+     * @param base64Str base64-encoded JSON
+     * @return decoded object if input is non-empty/non-null.  Null if given null/empty input.
+     */
+     public static decodeSfContext(base64Str: string): SfContext {
         if (base64Str == null || base64Str.trim().length === 0) {
             return null;
         }
@@ -28,29 +33,63 @@ export class ContextUtil {
         return obj;
     }
 
-    public static encodeSfContext(obj: SfContext): string {
+    /**
+     * Encode a SfContext to base64-encoded JSON.
+     * @param obj Salesforce Context, including UserContext.
+     * @return base64-encoded JSON string if input is non-null.  Null if given null input.
+     */
+     public static encodeSfContext(obj: SfContext): string {
         if (obj == null) {
             return null;
         }
         return Buffer.from(JSON.stringify(obj), "utf8").toString("base64");
     }
 
-    public static decodeSfFnContext(base64Str: string): SfFnContext {
+    /**
+     * Decode a SfFnContext from its base64-encoded JSON.  Note: will parse `deadline` string
+     * in ISO8601 format to a JS `Date` object when set in incoming JSON object.
+     * @param base64Str base64-encoded JSON
+     * @return decoded object if input is non-empty/non-null.  Null if given null/empty input.
+     */
+     public static decodeSfFnContext(base64Str: string): SfFnContext {
         if (base64Str == null || base64Str.trim().length === 0) {
             return null;
         }
         var obj: SfFnContext = JSON.parse(Buffer.from(base64Str, "base64").toString("utf8"));
+        if (obj.deadline != null && typeof obj.deadline === "string") {
+            var dt: Date = new Date(Date.parse(obj.deadline));
+            obj.deadline = dt;
+        }
         return obj;
     }
 
-    public static encodeSfFnContext(obj: SfFnContext): string {
+    /**
+     * Encode a SfFnContext to base64-encoded JSON.  Note: will format `deadline` as ISO8601
+     * string to Seconds level in UTC with "Z" suffix.
+     * @param obj Salesforce Function Context.
+     * @return base64-encoded JSON string if input is non-null.  Null if given null input.
+     */
+     public static encodeSfFnContext(obj: SfFnContext): string {
         if (obj == null) {
             return null;
         }
-        return Buffer.from(JSON.stringify(obj), "utf8").toString("base64");
+        // Make a copy so we can format `deadline` if populated
+        var objCopy: Object = Object.assign({}, obj);
+        if (obj.deadline != null && obj.deadline instanceof Date) {
+            var isoStr = obj.deadline.toISOString();
+            isoStr = isoStr.slice(0,-5)+"Z" // remove .000 milliseconds part
+            objCopy['deadline'] = isoStr;
+        }
+        return Buffer.from(JSON.stringify(objCopy), "utf8").toString("base64");
     }
 
-    public static decodeExtraInfo(urlEncStr: string): ResponseExtraInfo {
+    /**
+     * Decode a ResponseExtraInfo from its URL-encoded UTF-8 JSON string.
+     * Note use of URL encoding for this object rather than base64.
+     * @param urlEncStr URL-encoded UTF-8 string.
+     * @return decoded object if input is non-empty/non-null, Null if given null/empty input.
+     */
+     public static decodeExtraInfo(urlEncStr: string): ResponseExtraInfo {
         if (urlEncStr == null || urlEncStr.trim().length === 0) {
             return null;
         }
@@ -58,7 +97,12 @@ export class ContextUtil {
         return obj;
     }
 
-    public static encodeExtraInfo(obj: ResponseExtraInfo): string {
+    /**
+     * Encode a ResponseExtraInfo to URL-encoded UTF-8 JSON.
+     * @param obj Response Extra Info object.
+     * @return URL-encoded UTF-9 string if input is non-null.  Null if given null input.
+     */
+     public static encodeExtraInfo(obj: ResponseExtraInfo): string {
         if (obj == null) {
             return null;
         }
--- node/test/index.test.ts
+++ node/test/index.test.ts
@@ -24,9 +24,6 @@ import {
 } from "../src";
 import { assert } from 'chai';
 
-/**
- * ContextUtil unit tests.
- */
  const CE_SFCONTEXT = "eyJhcGlWZXJzaW9uIjoiNTAuMCIsInBheWxvYWRWZXJzaW9uIjoiMC4x" +
     "IiwidXNlckNvbnRleHQiOnsib3JnSWQiOiIwMER4eDAwMDAwMDZJWUoiLCJ1c2VySWQiOiIwMDV4eDAwMDA" +
     "wMVg4VXoiLCJvbkJlaGFsZk9mVXNlcklkIjpudWxsLCJ1c2VybmFtZSI6InRlc3QtenFpc25mNnl0bHF2QG" +
@@ -51,27 +48,88 @@ import { assert } from 'chai';
     "k%22%3A%22com/example/fn/MyFunc.java%3A29%5Cn%20java/lang/RuntimeException.java%3A1" +
     "11%22%7D";
 
-describe('Unit Tests', () => {
+/**
+ * ContextUtil unit tests.
+ */
+ describe('Unit Tests', () => {
 
-   it("testSfContext", () => {
-      var ctx: SfContext = ContextUtil.decodeSfContext(CE_SFCONTEXT);
-      assert.isNotNull(ctx);
-      assert.isNotNull(ctx.apiVersion);
-      assert.isNotNull(ctx.payloadVersion);
-      assert.isNotNull(ctx.userContext);
+  it("testSfContext", () => {
+    var ctx: SfContext = ContextUtil.decodeSfContext(CE_SFCONTEXT);
+    assert.isNotNull(ctx);
+    assert.isNotNull(ctx.apiVersion);
+    assert.isNotNull(ctx.payloadVersion);
+    assert.isNotNull(ctx.userContext);
 
-      assert.equal("50.0", ctx.apiVersion);
-      assert.equal("0.1", ctx.payloadVersion);
-      assert.equal("00Dxx0000006IYJ", ctx.userContext.orgId);
-      assert.equal("005xx000001X8Uz", ctx.userContext.userId);
-      assert.isNull(ctx.userContext.onBehalfOfUserId);
-      assert.equal("test-zqisnf6ytlqv@example.com", ctx.userContext.username);
-      var urlStr: string = "http://pistachio-virgo-1063-dev-ed.localhost.internal.salesforce.com:6109";
-      assert.equal(urlStr, ctx.userContext.salesforceBaseUrl);
-      assert.equal(urlStr, ctx.userContext.orgDomainUrl);
+    assert.equal("50.0", ctx.apiVersion);
+    assert.equal("0.1", ctx.payloadVersion);
+    assert.equal("00Dxx0000006IYJ", ctx.userContext.orgId);
+    assert.equal("005xx000001X8Uz", ctx.userContext.userId);
+    assert.isNull(ctx.userContext.onBehalfOfUserId);
+    assert.equal("test-zqisnf6ytlqv@example.com", ctx.userContext.username);
+    var urlStr: string = "http://pistachio-virgo-1063-dev-ed.localhost.internal.salesforce.com:6109";
+    assert.equal(urlStr, ctx.userContext.salesforceBaseUrl);
+    assert.equal(urlStr, ctx.userContext.orgDomainUrl);
 
-      var reEncoded: string = ContextUtil.encodeSfContext(ctx);
-      assert.equal(CE_SFCONTEXT, reEncoded);
-   });
+    var reEncoded: string = ContextUtil.encodeSfContext(ctx);
+    assert.equal(CE_SFCONTEXT, reEncoded);
+  });
+
+  it("testSfFnContext", () => {
+    var ctx: SfFnContext = ContextUtil.decodeSfFnContext(CE_SFFNCONTEXT);
+    assert.isNotNull(ctx);
+    assert.isNotNull(ctx.accessToken);
+    assert.isNotNull(ctx.functionInvocationId);
+    assert.isNotNull(ctx.proxyClientToken);
+    assert.isNotNull(ctx.functionName);
+    assert.isNotNull(ctx.apexId);
+    assert.isNotNull(ctx.apexFQN);
+    assert.isNotNull(ctx.requestId);
+    assert.isNotNull(ctx.resource);
+    assert.isNotNull(ctx.asyncResponseCallbackPath);
+    assert.isNotNull(ctx.deadline);
+
+    var accTok: string = "00Dxx0000006IYJ!AQEAQNRaAQEAQNRaAQEAQNRaAQEAQNRaAQEAQNRaAQEAQNRaAQEAQNRaAQEAQNRaAQEAQNRaAQEAQNRa";
+    assert.equal(accTok, ctx.accessToken);
+    assert.equal("9md6u0000000LfxAAE", ctx.functionInvocationId);
+    assert.equal("sf-fx-proxy9md6u0000000LfxAAE-sfdZz19vILmZz19", ctx.proxyClientToken);
+    assert.equal("MyProject.testfn1", ctx.functionName);
+    assert.equal("a0cM00000043ST4", ctx.apexId);
+    assert.equal("classes/FunctionApex.cls:14", ctx.apexFQN);
+    assert.equal("00Dxx0000006IYJEA2-9md6u0000000LfxAAE-1a893e50", ctx.requestId);
+    assert.equal("testfn1-anteater-d99.basin-dd1dd1.evergreen.space", ctx.resource);
+    assert.equal("/services/function/arh", ctx.asyncResponseCallbackPath);
+    var february = 1; // Ugh, JS 0-based months
+    var dt: Date = new Date(Date.UTC(2022, february, 10, 17, 59, 11, 0));
+    assert.equal(dt.toISOString(), ctx.deadline.toISOString());
+
+    var reEncoded: string = ContextUtil.encodeSfFnContext(ctx);
+    assert.equal(CE_SFFNCONTEXT, reEncoded);
+  });
+
+  it("testExtraInfo", () => {
+    var xi: ResponseExtraInfo = ContextUtil.decodeExtraInfo(RESPONSE_EXTRA_INFO);
+    assert.isNotNull(xi);
+    assert.isNotNull(xi.requestId);
+    assert.isNotNull(xi.resource);
+    assert.isNotNull(xi.source);
+    assert.isNotNull(xi.execTimeMs);
+    assert.isNotNull(xi.statusCode);
+    assert.isNotNull(xi.isFunctionError);
+    assert.isNotNull(xi.stack);
+
+    assert.equal("00Dxx0000006IYJEA2-9md6u0000000LfxAAE-1a893e50", xi.requestId);
+    assert.equal("testfn1-anteater-d99.basin-dd1dd1.evergreen.space", xi.resource);
+    assert.equal("com/example/fn/MyFunc.java:29", xi.source);
+    assert.equal(101.393, xi.execTimeMs);
+    assert.equal(500, xi.statusCode);
+    assert.equal(true, xi.isFunctionError);
+    assert.equal("com/example/fn/MyFunc.java:29\n java/lang/RuntimeException.java:111", xi.stack);
+
+    var reEncoded: string = ContextUtil.encodeExtraInfo(xi);
+    // JS encodeURIComponent escapes slashes (/) but python3 urllib (created sample above) does not.
+    let re = /[/]/gi;
+    var expected: string = RESPONSE_EXTRA_INFO.replace(re, "%2F");
+    assert.equal(expected, reEncoded);
+  });
 
 });
